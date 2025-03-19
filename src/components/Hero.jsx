b68a842ecc294cec2eb6d508c72f606d8018aadb
Pablofu9 <pfuertes.ruiz.96@gmail.com>
2025-03-19 13:12:57 +0100

    Finish download CV

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
-import HeroImage from "../assets/images/imagenCara.jpg"
+import HeroImage from "../assets/images/imagenCara.jpg";
+import { useState } from "react";
+
 const Hero = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleScroll = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+  function handleNav() {
+    setNav(!nav);
+  }
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 w-full mx-auto h-auto py-8 px-4">
       <div className="col-span-1 my-auto mx-auto w-[300px] h-[300px] lg:w-[300px] ">
@@ -34,16 +48,21 @@ const Hero = () => {
         <div className="flex flex-row">
           <div className="my-8">
             <a
-              href="/"
+              href="/CV_PabloFuertes.pdf" // Ruta al archivo de tu CV
               className="px-6 py-3 w-full rounded-xl mr-4 text-white bg-gradient-orange-red bg-[length:200%] bg-left transition-all duration-500 ease-in-out hover:bg-right"
+              download // Este atributo indica que el enlace es para descargar el archivo
             >
               Descargar CV
             </a>
           </div>
           <div className="my-8">
             <a
-              href="/"
+              href="#contact" // Esto se asegura de que se desplace a la sección con el id "contact"
               className="px-6 py-3 w-full rounded-xl mr-4 text-white bg-transparent bg-[length:200%] transition-all duration-500 ease-in-out border hover:bg-white hover:text-black"
+              onClick={(e) => {
+                e.preventDefault(); // Prevenir comportamiento por defecto
+                handleScroll("contact"); // Función para desplazarse suavemente a la sección
+              }}
             >
               Contactar
             </a>
