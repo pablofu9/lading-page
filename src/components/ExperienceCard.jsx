18d9dab2435f72b495f2dd7672a3e4b6f7ecf778
Pablofu9 <pfuertes.ruiz.96@gmail.com>
2025-03-25 20:00:23 +0100

    change experience b

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -10,7 +10,7 @@ const ExperienceCard = ({
   techIcon: TechIconComponent // Nuevo prop para el icono tecnológico
 }) => {
   return (
-    <div className="bg-darkGray shadow-lg rounded-lg p-10 mx-auto w-auto h-auto shadow-gray-500/10 text-justify">
+    <div className="bg-black shadow-lg rounded-lg p-10 mx-auto w-auto h-auto shadow-gray-500/10 text-justify border-white border">
       <div className="flex items-center mb-4">
         {/* Icono tecnológico (si se pasa) */}
         {TechIconComponent && <TechIcon icon={TechIconComponent}/>}
